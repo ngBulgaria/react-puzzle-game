a1c407a6e6844592b0e8ebc9dab47b0367bb40f1
Nikolay Georgiev <49595483+ngBulgaria@users.noreply.github.com>
2024-08-25 11:38:37 +0000

    Updates

---
+++ precache-manifest.cfb83e0fa279cda367699197ad2e5446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae7998ba5d8eabfbf54e1dca11a69e7",
+    "url": "/react-puzzle-game/index.html"
+  },
+  {
+    "revision": "1c03d338cfe6b2801539",
+    "url": "/react-puzzle-game/static/css/main.5a49c3a8.chunk.css"
+  },
+  {
+    "revision": "cb94078f0ed4cf8bf980",
+    "url": "/react-puzzle-game/static/js/2.990dd9f0.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/react-puzzle-game/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "1c03d338cfe6b2801539",
+    "url": "/react-puzzle-game/static/js/main.fa5a77d8.chunk.js"
+  },
+  {
+    "revision": "9d515f3719ba88162471",
+    "url": "/react-puzzle-game/static/js/runtime~main.00f2f37f.js"
+  }
+]);
